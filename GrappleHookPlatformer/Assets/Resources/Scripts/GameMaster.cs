1ec6542c6b97563ec960dc0d535ab37b9a51d863
KenWhy-PC\KenWhy <e0003284@u.nus.edu>
2017-11-06 18:49:33 +0800

    Respawns player after 2 seconds after killing them

--- GrappleHookPlatformer/Assets/Resources/Scripts/GameMaster.cs
+++ GrappleHookPlatformer/Assets/Resources/Scripts/GameMaster.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
+    public static GameMaster gameMaster;
+
+    private void Start() {
+        if(gameMaster == null) {
+            gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+        }
+    }
+
+    //Vars for spawning player;
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    public IEnumerator RespawnPlayer() {
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
     public static void KillPlayer(Player player) {
         Destroy(player.gameObject);
+        gameMaster.StartCoroutine(gameMaster.RespawnPlayer());
     }
 
 }
