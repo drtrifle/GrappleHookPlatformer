0e5fe8c6319ad5c3606d3062e799a1ee21987da1
KenWhy-PC\KenWhy <e0003284@u.nus.edu>
2017-11-06 21:17:03 +0800

    Added Respawn Effect with sound

--- GrappleHookPlatformer/Assets/Resources/Scripts/GameMaster.cs
+++ GrappleHookPlatformer/Assets/Resources/Scripts/GameMaster.cs
@@ -10,17 +10,24 @@ public class GameMaster : MonoBehaviour {
         if(gameMaster == null) {
             gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
         }
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     //Vars for spawning player;
     public Transform playerPrefab;
     public Transform spawnPoint;
     public int spawnDelay = 2;
+    public GameObject spawnPrefab; //Prefab of particle effects when player spawns
+    AudioSource audioSource;
 
     public IEnumerator RespawnPlayer() {
+        audioSource.Play();
         yield return new WaitForSeconds(spawnDelay);
-
         Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        GameObject particleClone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation);
+        Destroy(particleClone, 3f);
+
     }
 
     public static void KillPlayer(Player player) {
