d5e3c95ddb7eb91640266eee127ee5d3db4366fa
KenWhy-PC\KenWhy <e0003284@u.nus.edu>
2017-11-06 12:09:26 +0800

    Created script to kill player when they fall out of world

---
+++ GrappleHookPlatformer/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [System.Serializable]
+    public class PlayerStats {
+        public int health = 100;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary = -20;
+
+    void Update() {
+        //Kill player if they fell outside the world
+        if (transform.position.y <= fallBoundary) {
+            DamagePlayer(9999999);
+        }
+    }
+
+    //Deals damage to player's health
+    public void DamagePlayer(int damage) {
+        playerStats.health -= damage;
+
+        if(playerStats.health <= 0) {
+            GameMaster.KillPlayer(this);
+        }
+    } 
+}
