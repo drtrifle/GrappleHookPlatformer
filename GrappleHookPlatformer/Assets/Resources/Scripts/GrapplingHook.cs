f73b041233a50ea6450f0fa17ba401b7aa3d020f
KenWhy-PC\KenWhy <e0003284@u.nus.edu>
2017-11-08 00:02:47 +0800

    Added Simple GrappleHook to new scene

---
+++ GrappleHookPlatformer/Assets/Resources/Scripts/GrapplingHook.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour {
+
+    public LineRenderer line;
+    DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    public float distance = 10f;
+    public LayerMask mask;
+    public float step = 0.02f;
+
+    private bool isHooked = false;
+
+    // Use this for initialization
+    void Start() {
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+        line.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+        if (joint.distance <= .5f) {
+            line.enabled = false;
+            joint.enabled = false;
+            isHooked = false;
+        }
+
+        if (isHooked) {
+            line.SetPosition(0, transform.position);
+        }
+
+
+        if (Input.GetButtonDown("Fire1")) {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
+
+            if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null) {
+                joint.enabled = true;
+                Vector2 connectPoint = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+                connectPoint.x = connectPoint.x / hit.collider.transform.localScale.x;
+                connectPoint.y = connectPoint.y / hit.collider.transform.localScale.y;
+                Debug.Log(connectPoint);
+                joint.connectedAnchor = connectPoint;
+
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.distance = Vector2.Distance(transform.position, hit.point);
+
+                //LineRenderer
+                line.enabled = true;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, hit.point);
+                isHooked = true;
+
+            }
+        }
+
+        if (Input.GetButton("Fire2")) {
+            joint.distance -= step;
+        }
+
+    }
+}
