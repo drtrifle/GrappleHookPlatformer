c49f9c1d536c71f06cef233f6e570cba3fc1d1d1
KenWhy-PC\KenWhy <e0003284@u.nus.edu>
2017-11-14 00:18:21 +0800

    Brought back old grappleHook Script as new one was too complex for game

---
+++ GrappleHookPlatformer/Assets/Resources/Scripts/GrapplingHook.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GrapplingHook : MonoBehaviour {
+
+    public GameObject hookPrefab;
+    private GameObject hookObject;
+
+    private LineRenderer line;
+    private DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    public float distance = 10f;
+    public LayerMask mask;
+    public float step = 0.02f;
+
+    private bool isHooked = false;
+
+    // Use this for initialization
+    void Start() {
+        joint = GetComponent<DistanceJoint2D>();
+        line = GetComponent<LineRenderer>();
+
+        Assert.IsNotNull(joint, "DistanceJoint is null");
+        Assert.IsNotNull(line, "LineRenderer is null");
+
+        joint.enabled = false;
+        line.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+        //Break rope when distance too small
+        if (joint.distance <= .5f) {
+            DisableHook();
+        }
+
+        //Update LineRenderer
+        if (isHooked) {
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, hookObject.transform.position);
+        }
+
+        //Fire GrappleHook
+        if (Input.GetButtonDown("Fire1")) {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
+
+            if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null) {
+                joint.enabled = true;
+                Vector2 connectPoint = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+                connectPoint.x = connectPoint.x / hit.collider.transform.localScale.x;
+                connectPoint.y = connectPoint.y / hit.collider.transform.localScale.y;
+                Debug.Log(connectPoint);
+
+                //Destroy hook if alreay exists
+                if (hookObject != null) {
+                    Destroy(hookObject);
+                }
+
+                hookObject = Instantiate(hookPrefab, new Vector3(hit.point.x, hit.point.y, 0), Quaternion.identity, hit.transform);
+                joint.connectedAnchor = connectPoint;
+
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.distance = Vector2.Distance(transform.position, hit.point);
+
+                //LineRenderer
+                line.enabled = true;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, hookObject.transform.position);
+                isHooked = true;
+            }else {
+                DisableHook();
+            }
+        }
+
+        //Allow Player to move up/down rope when hooked
+        joint.distance = joint.distance - (step * Input.GetAxis("Vertical"));
+
+    }
+
+    private void DisableHook() {
+        line.enabled = false;
+        joint.enabled = false;
+        isHooked = false;
+
+        //Destroy hook if alreay exists
+        if (hookObject != null) {
+            Destroy(hookObject);
+        }
+    }
+}
