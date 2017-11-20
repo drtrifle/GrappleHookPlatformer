b623864eae2ed7343ec68d12e7c2f81ea8f813c7
KenWhy-PC\KenWhy <e0003284@u.nus.edu>
2017-11-20 18:17:01 +0800

    Created Fly Enemy Script

---
+++ GrappleHookPlatformer/Assets/Resources/Scripts/Enemy Scripts/FlyEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class FlyEnemy : Enemy {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void OnTriggerEnter2D(Collider2D collider) {
+        
+    }
+}
